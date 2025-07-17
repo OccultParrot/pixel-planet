da70c729d51f43e92a0a8089200e0c7fa2e5a1a9
Thomas Stemler <stemlertho@gmail.com>
2025-07-17 08:08:15 -0500

    Made clouds change with time

--- scripts/clouds.gd
+++ scripts/clouds.gd
@@ -3,4 +3,5 @@ extends MeshInstance3D
 @export_range(0, 100) var rotation_speed: float = 2
 
 func _physics_process(delta: float) -> void:
-	rotate(Vector3(0, 1, 0).normalized(), rotation_speed / 1000)
+	#rotate(Vector3(0, 1, 0).normalized(), rotation_speed / 1000)
+	pass
