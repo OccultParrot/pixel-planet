6450f48aceece19bbdc0e36f7ebaea624d358324
Thomas Stemler <stemlertho@gmail.com>
2025-07-17 21:17:04 -0500

    Added 'dist_to_equator', and for now we just are drawing a slightly lighter color on top. Also added bumpy mountains.

--- scripts/clouds.gd
+++ scripts/clouds.gd
@@ -3,5 +3,4 @@ extends MeshInstance3D
 @export_range(0, 100) var rotation_speed: float = 2
 
 func _physics_process(delta: float) -> void:
-	#rotate(Vector3(0, 1, 0).normalized(), rotation_speed / 1000)
-	pass
+	rotate(Vector3(0, 1, 0).normalized(), rotation_speed / 1000)
